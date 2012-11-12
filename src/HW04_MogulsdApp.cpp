7c465e94a4e996fa252fc5009ce3bf83fa07376e
Scott <mogulsd@muohio.edu>
2012-11-11 19:28:15 -0500

    trying to get the map to show up
    attempting to get the map to show up and then draw dots over the
    starbucks locations

--- src/HW04_MogulsdApp.cpp
+++ src/HW04_MogulsdApp.cpp
@@ -1,5 +1,7 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
 #include "Starbucks.h"
 #include "mogulsdStarbucks.h"
 #include <iostream>
@@ -19,15 +21,27 @@ class HW04_MogulsdApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
-	
+	ifstream myfile;
+	string words;
+	Entry* starbucksLocations;
+private:
+	static const int AppWidth=1000;
+	static const int AppHeight=673;
+	static const int TextureSize=1024;
+	Surface* mySurface;
+	uint8_t* marker;
+	gl::Texture Map;
+	Vec2f center;
+
 };
 
 void HW04_MogulsdApp::setup()
 {
-	
-	ifstream myfile;
-	myfile.open("../resources/Starbucks_2006.csv");
-	string words="";
+	Map = gl::Texture(loadImage( "usa-map.gif"));
+	starbucksLocations = new Entry[7700];
+	myfile.open("Starbucks_2006.csv");
+	words="";
+	int i = 0;
 	if(myfile.fail())
 	{
 		cout << "Error: main() ; Failed to open file: " << endl;
@@ -35,15 +49,22 @@ void HW04_MogulsdApp::setup()
 	}
 	else
     {
-        string line;
-        do
-        {
-            getline(myfile,line,',');
-            
-        } while (!myfile.eof());
-
-        myfile.close();
-    }
+		while(!myfile.eof()){
+			getline(myfile, starbucksLocations[i].identifier,',');
+			myfile>>starbucksLocations[i].x;
+			myfile.get();
+			myfile>>starbucksLocations[i].y;
+			myfile.get();
+			cout<<starbucksLocations[i].identifier<<endl;
+			i++;
+		}
+		myfile.close();
+		mogulsdStarbucks* starbucks = new mogulsdStarbucks();
+		starbucks ->build(starbucksLocations,7655);
+		Entry* nearest = starbucks ->getNearest(.1,.1);
+		cout<<nearest->identifier<<" "<<nearest->x<<" "<<nearest->y<<endl;
+	}
+	
 }
 
 void HW04_MogulsdApp::mouseDown( MouseEvent event )
@@ -56,8 +77,14 @@ void HW04_MogulsdApp::update()
 
 void HW04_MogulsdApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::color( Color(0,0,0));
+	gl::draw(Map, getWindowBounds());
+
+	for(int i = 0; i < 7655; i++){
+		center = Vec2f(starbucksLocations[i].x,1-(starbucksLocations[i].y));
+		gl::color(40,140,9);
+		gl::drawSolidCircle(center,5);
+	}
 }
 
 CINDER_APP_BASIC( HW04_MogulsdApp, RendererGl )
